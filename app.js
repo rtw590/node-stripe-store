53a2967e3597c1ebc0a5d213312a5d85b47eaab1
Rob Waller <r.t.waller@gmail.com>
2018-04-05 11:14:11 -0500

    Fully working before deployment

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+
+// Charge route
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+    .then(customer => stripe.charges.create({
+        amount,
+        description: 'Web Development ebook',
+        currency: 'usd',
+        customer: customer.id
+    }))
+    .then(charge => res.render('success'));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
